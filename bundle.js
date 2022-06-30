a8bef5b57eac0ccbcb765fb55f244580abd74da5
XiangNorth <i@xiangnorth.com>
2022-07-01 06:41:10 +0800

    update: first available version

---
+++ bundle.js
@@ -0,0 +1,20 @@
+const { build } = require("esbuild");
+const { stylusLoader } = require("esbuild-stylus-loader");
+const fs = require("fs");
+
+build({
+  entryPoints: ["./lib/index.js"],
+  bundle: true,
+  minify: false,
+  outdir: "./source",
+  plugins: [
+    stylusLoader({
+      stylusOptions: {
+        includeCss: true,
+      },
+    }),
+  ],
+}).then(() => {
+  fs.rename("./source/index.js", "./source/js/sagirix.min.js", () => {});
+  fs.rename("./source/index.css", "./source/css/sagirix.min.css", () => {});
+});
